9ba1a611b22ef6b3d9146a0621d3d16c09087e9d
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-02-04 20:36:58 +0200

    fix warnings

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,11 @@ import React, { useState } from "react";
 
 import ImageGrid from "./comps/ImageGrid";
 import Modal from "./comps/Modal";
-import UploadForm from "./comps/UploadForm";
 
 const Home = () => {
   const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
-      {/* <Title /> */}
-      {/* <UploadForm /> */}
       <ImageGrid setSelectedImg={setSelectedImg} />
       {selectedImg && (
         <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
