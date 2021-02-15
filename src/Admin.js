823df2e5e76b19a23826056cc422e4e569c4251d
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-02-15 11:04:39 +0200

    admin routing

--- src/Admin.js
+++ src/Admin.js
@@ -5,9 +5,11 @@ import "firebase/auth";
 import UploadForm from "./comps/UploadForm";
 import DashBoardTitle from "./comps/DashboardTitle";
 import { Box } from "@chakra-ui/react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 
 const Admin = () => {
   const [isValid, setIsValid] = useState(false);
+  let { path } = useRouteMatch();
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       setIsValid(true);
@@ -16,10 +18,22 @@ const Admin = () => {
   return (
     <div className="Admin">
       {!isValid && <LoginForm />}
-      <Box m={2}>
-        {isValid && <DashBoardTitle setIsValid={setIsValid} />}
-        {isValid && <UploadForm />}
-      </Box>
+      {isValid && (
+        <Box m={2}>
+          <DashBoardTitle setIsValid={setIsValid} />
+          <Switch>
+            <Route exact path={path}>
+              <h2>main</h2>
+            </Route>
+            <Route path={`${path}/uploadform`}>
+              <UploadForm />
+            </Route>
+            <Route path={`${path}/deletephoto`}>
+              <h2>Dete photo</h2>
+            </Route>
+          </Switch>
+        </Box>
+      )}
     </div>
   );
 };
